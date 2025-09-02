25de1df403942012267b9251fffdec489b4834c7
Aidan Killeen <aidan.killeen@gmail.com>
2025-09-02 14:59:03 +0100

    added parameter_introduction

---
+++ parameter_introduction.py
@@ -0,0 +1,15 @@
+# parameter_introduction.py
+
+names = ['Alice', 'Bob', 'Charlie', 'Donna']
+
+for name in names:
+    print (f"{name}", end=" -> ")
+
+
+print (names[0], names[1], names[2], names[3], sep=", ") 
+
+
+v = "99"
+
+val = int(v)
+r = range()
