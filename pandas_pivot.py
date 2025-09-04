cb99ef3e61d4b23b71d0898e9a1a8ce735a5c59a
Aidan Killeen <aidan.killeen@gmail.com>
2025-09-04 13:38:15 +0100

    finished pivot table

--- pandas_pivot.py
+++ pandas_pivot.py
@@ -5,11 +5,13 @@ import pandas as pd
 
 df_sales = pd.read_csv("sales.csv")
 
-print (df_sales.head())
-
-
 df_pivot = pd.pivot_table(df_sales, index="product", columns="colour", values="quantity", aggfunc="sum", fill_value=0)
-
 df_pivot['totals'] = df_pivot.sum(axis=1)
-print (df_pivot)
+
+totals_row = df_pivot.sum(axis=0).to_frame().T
+totals_row.index = ["Totals"]
+
+pivot_with_totals = pd.concat([df_pivot, totals_row])
+
+print (pivot_with_totals)
 
