967652715507ba7107250529afb57037ef214680
Aidan Killeen <aidan.killeen@gmail.com>
2025-09-04 14:04:31 +0100

    started ui app

---
+++ gui_introduction.py
@@ -0,0 +1,26 @@
+# gui_introduction.py
+
+
+import tkinter as tk
+from tkinter import messagebox
+
+main_window = tk.Tk()
+main_window.title("Python GUI")
+main_window.geometry("600x400")
+
+
+def on_click():
+    messagebox.showinfo("Information", "You clicked the button")
+
+button = tk.Button(main_window, text="Press Me", command=on_click)
+button.place(x=10, y=20)
+
+list_box = tk.Listbox(main_window, height=10)
+list_box.place(x=10, y=50)
+list_box.insert(tk.END, "one")
+list_box.insert(tk.END, "two")
+list_box.insert(tk.END, "three")
+
+main_window.mainloop()
+
+
