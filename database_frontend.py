8413d51aa16566544c09c163775f8f8a86b42d61
Aidan Killeen <aidan.killeen@gmail.com>
2025-09-04 14:42:20 +0100

    finished database ui

--- database_frontend.py
+++ database_frontend.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 
 from PersonDAO import PersonDAO
 
+id_map = {}
+
 main_window = tk.Tk()
 main_window.title("Database Frontend GUI")
 main_window.geometry("600x400")
@@ -20,17 +22,36 @@ button.place(x=10, y=20)
 list_box = tk.Listbox(main_window, height=10)
 list_box.place(x=10, y=50)
 
+selected_user = None
+
+
 def on_select(evt):
+    global selected_user
     selection = list_box.curselection()
     if selection:
         index = selection[0]
-        name = list_box.get(index)
-        print (name)
+        selected_user = id_map[index]
+        print (selected_user)
+
+def on_delete():
+    global selected_user
+    if selected_user != None:
+        answer = messagebox.askyesno("Confirmation", "Are you suer?")
+
+        if answer:
+            dao.delete(selected_user.id)
+
+            index = list_box.curselection()[0]
+            list_box.delete(index)
+
+delete_button = tk.Button(main_window, text="Delete", command=on_delete)
+delete_button.place(x=160, y=60)
 
 
 list_box.bind("<<ListboxSelect>>", on_select)
 
-for user in users:
+for index, user in enumerate(users):
+    id_map[index] = user
     list_box.insert(tk.END, user.name)
 
 main_window.mainloop()
